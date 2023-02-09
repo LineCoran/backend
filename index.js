134cb8b406b9b26fee0ad59513965cc0c42216e0
LineCoran <vppisya@gmail.com>
2023-02-09 19:29:46 +0300

    fix: fix everything

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 
 
-const PORT = 'backend-production-37e4.up.railway.app';
+const PORT = 5000;
 
-const app = express(PORT, () => {
-  console.log(`server was started on ${PORT}`)
-});
+const app = express();
 
+const start = ()  => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`server was started on ${PORT}`);
+    }
+    );
+  } catch(err) {
+    app.send(err);
+  }
+}
 
+app.use('/', (res, req) => {
+  res.send('welcome to server start page');
+})
+
+start();
